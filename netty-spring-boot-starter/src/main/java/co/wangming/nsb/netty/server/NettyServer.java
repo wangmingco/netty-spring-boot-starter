e7ca2d24f74b9936bc8927e66e09cbf35959d3d7
wangming01 <wangming01@finupgroup.com>
2020-01-19 16:37:13 +0800

    修改服务启动监听地址

--- netty-spring-boot-starter/src/main/java/co/wangming/nsb/netty/server/NettyServer.java
+++ netty-spring-boot-starter/src/main/java/co/wangming/nsb/netty/server/NettyServer.java
@@ -17,6 +17,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 /**
  * Created By WangMing On 2019-12-06
  **/
@@ -60,7 +65,7 @@ public class NettyServer {
             if (springBootNettyProperties.getAddress() != null) {
                 bindChannelFuture = b.bind(springBootNettyProperties.getAddress(), port).sync();
             } else {
-                bindChannelFuture = b.bind(port).sync();
+                bindChannelFuture = b.bind(getServerIp(), port).sync();
             }
 
             bindChannelFuture.sync();
@@ -72,6 +77,38 @@ public class NettyServer {
         } catch (InterruptedException e) {
             log.error("", e);
             stop();
+        }
+    }
+
+    public static String getServerIp() {
+        String localip = null;// 本地IP，如果没有配置外网IP则返回它
+        String netip = null;// 外网IP
+        try {
+            Enumeration netInterfaces = NetworkInterface.getNetworkInterfaces();
+            boolean finded = false;// 是否找到外网IP
+            while (netInterfaces.hasMoreElements() && !finded) {
+                NetworkInterface ni = (NetworkInterface) netInterfaces.nextElement();
+                Enumeration address = ni.getInetAddresses();
+                while (address.hasMoreElements()) {
+
+                    InetAddress ip = (InetAddress) address.nextElement();
+                    if (!ip.isSiteLocalAddress() && !ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {// 外网IP
+                        netip = ip.getHostAddress();
+                        finded = true;
+                        break;
+                    } else if (ip.isSiteLocalAddress() && !ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {// 内网IP
+                        localip = ip.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            return "127.0.0.1";
+        }
+
+        if (netip != null && !"".equals(netip)) {
+            return netip;
+        } else {
+            return localip;
         }
     }
 
