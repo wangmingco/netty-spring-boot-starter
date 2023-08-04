234eb581d448e335bda118f2d39acf7f781572a9
wangming <wangming.co@hotmail.com>
2023-08-04 17:10:05 +0800

    修改protobuf生成方案

--- protobuf/compile_csharp.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SRC_DIR=./protos/
-DST_DIR=../clients/csharp-client/csharp-client
-
-./protoc-3.11.1-osx-x86_64/bin/protoc -I=$SRC_DIR --csharp_out=$DST_DIR $SRC_DIR/search.proto
